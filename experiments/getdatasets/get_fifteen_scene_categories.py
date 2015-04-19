9334a5fddf54dfc895b4303e878981bc70cc18a9
Sergey Romanov <xxsmotur@gmail.com>
2015-04-19 23:55:23 +0500

    Append new dataset

---
+++ experiments/getdatasets/get_fifteen_scene_categories.py
@@ -0,0 +1,26 @@
+import urllib.request
+import zipfile
+import os
+
+
+def get_fifteen_scene_categories(outdir):
+	""" Output dir after extraction """
+	name = 'scene_categories.zip'
+	path = 'http://www-cvr.ai.uiuc.edu/ponce_grp/data/scene_categories/' + name
+	if not os.path.exists(name):
+		data = urllib.request.urlopen(path)
+		with open(name, 'wb') as writer:
+			writer.write(data.read())
+	if not os.path.exists(name):
+		raise Exception("File {0} was not downloaded complete".format(name))
+	scene = zipfile.ZipFile(name)
+	for name in scene.namelist():
+		(dirname, filename) = os.path.split(name)
+		fullpath = outdir + '/' + dirname
+		if not os.path.exists(fullpath):
+			os.makedirs(fullpath)
+		if filename != '':
+			#scene.extract(filename, dirname)
+			with open(fullpath + '/' + filename, 'wb') as writer:
+				writer.write(scene.read(name))
+	scene.close()
