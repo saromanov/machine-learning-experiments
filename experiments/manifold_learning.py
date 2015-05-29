1fceecd8adaa46b40e35737dccb7f84e660742c8
Sergey Romanov <xxsmotur@gmail.com>
2015-05-29 23:56:07 +0500

    add some examples

--- experiments/manifold_learning.py
+++ experiments/manifold_learning.py
@@ -1,8 +1,15 @@
 from sklearn import manifold
 from sklearn.datasets import load_digits
 
+def isomap(X):
+    return manifold.Isomap(10, n_components=20).fit_transform(X)
+
+def tsne1(X, components):
+    return manifold.TSNE(n_components=components, learning_rate=100, n_iter=300).fit_transform(X)
+
+def tsne2(X, components):
+    return manifold.TSNE(n_components_components, learning_rate=1000, n_iter=400).fit_transform(X)
+
 digits = load_digits()
 X = digits.data
-y = digits.target
-
-iso = manifold.Isomap(10, n_components=2).fit_transform(X)
+#tsne1(X,2)
