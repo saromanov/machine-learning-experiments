f596c5ab43a37d92b18b465c34d01181fa4a509b
Sergey Romanov <xxsmotur@gmail.com>
2015-06-06 09:13:01 -0700

    Saving models

--- experiments/manifold_learning.py
+++ experiments/manifold_learning.py
@@ -2,14 +2,14 @@ from sklearn import manifold, svm
 from sklearn.datasets import load_digits
 from sklearn.cross_validation import train_test_split
 import matplotlib.pyplot as plt
-
+from save_load import save
 
 def isomap(X, components):
     return manifold.Isomap(10, n_components=components).fit_transform(X)
 
 
 def tsne1(X, components):
-    return manifold.TSNE(n_components=components, n_iter=300).fit_transform(X)
+    return manifold.TSNE(n_components=components, random_state=1234).fit_transform(X)
 
 
 def tsne2(X, components):
@@ -34,17 +34,23 @@ def create_model2(X, y, msg):
 	pass
 
 
-digits = load_digits()
-X = digits.data
-y = digits.target
-newX = isomap(X, 50)
-create_model(newX, y, "With Isomap and 50 comonents")
-newX2 = isomap(X, 20)
-create_model(newX2, y, "With Isomap and 20 comonents")
-newX3 = mds1(X, 20)
-create_model(newX3, y, "With MDS and 20 components")
-newX4 = tsne1(X,20)
-create_model(newX4, y, "With t-SNE and 20 components")
-newX5 = se(X, 2)
-create_model(newX5, y, "With Spectral Embedding and 5 components")
-create_model(X, y, "SVM, but without dimensionality reduction")
+def getAndSave():
+	digits = load_digits()
+	X = digits.data
+	y = digits.target
+	newX = isomap(X, 50)
+	save(newX, "./models/isomap50.pkl")
+	create_model(newX, y, "With Isomap and 50 comonents")
+	newX2 = isomap(X, 20)
+	save(newX2, "./models/isomap20.pkl")
+	create_model(newX2, y, "With Isomap and 20 comonents")
+	newX3 = mds1(X, 20)
+	save(newX3, "./models/mds20.pkl")
+	create_model(newX3, y, "With MDS and 20 components")
+	newX4 = tsne1(X,20)
+	create_model(newX4, y, "With t-SNE and 20 components")
+	newX5 = se(X, 2)
+	create_model(newX5, y, "With Spectral Embedding and 5 components")
+	create_model(X, y, "SVM, but without dimensionality reduction")
+
+getAndSave()
