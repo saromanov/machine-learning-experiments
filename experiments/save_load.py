410afcc4a7c33c8bd93025058701abf77a55a1e0
Sergey Romanov <xxsmotur@gmail.com>
2015-06-06 09:12:43 -0700

    Update

--- experiments/save_load.py
+++ experiments/save_load.py
@@ -1,7 +1,7 @@
 from sklearn.externals import joblib
 
-def save(path):
-	joblib.dump(newX, path, compress=9)
+def save(model, path):
+	joblib.dump(model, path, compress=9)
 
 def load(path):
 	return joblib.load(path)
