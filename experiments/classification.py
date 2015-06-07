6dbd62389d95097f7b70504f56879649712ea609
Sergey Romanov <xxsmotur@gmail.com>
2015-06-07 05:17:51 -0700

    update

--- experiments/classification.py
+++ experiments/classification.py
@@ -1,5 +1,7 @@
 from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier, BaggingClassifier
 from sklearn.datasets import load_digits
+from sklearn.datasets import fetch_mldata
+from sklearn.cross_validation import train_test_split
 
 
 def ABC(Xtrain, ytrain, Xtest):
@@ -16,3 +18,9 @@ def BC(Xtrain, ytrain, Xtest):
 	model = BaggingClassifier(bootsrap=True)
 	model.fit(Xtrain, ytrain)
 	return model.predict(Xtest)
+
+digits = fetch_mldata('global-earthquakes')
+X = digits.data
+y = digits.target
+train_data, test_data, train_labels, test_labels = train_test_split(X,  y[1], test_size=0.2, random_state=124)
+GBC(train_data, train_labels, test_labels)
