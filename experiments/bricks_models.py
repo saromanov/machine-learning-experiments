cdb3562fb116dcd2ed0c6c1942afb64c4e4013c1
Sergey Romanov <xxsmotur@gmail.com>
2015-07-10 09:26:22 -0700

    Example with conv net

--- experiments/bricks_models.py
+++ experiments/bricks_models.py
@@ -1,5 +1,5 @@
 from blocks.bricks import Linear, Softmax, Logistic, MLP, Rectifier
-from blocks.bricks.conv import ConvolutionalLayer, ConvolutionalSequence, MaxPooling
+from blocks.bricks.conv import ConvolutionalLayer, ConvolutionalSequence, MaxPooling, Flattener
 from blocks.algorithms import GradientDescent, Momentum, AdaGrad, AdaDelta, Scale
 from blocks.bricks.cost import CategoricalCrossEntropy, SquaredError
 from blocks.initialization import IsotropicGaussian, Constant
@@ -19,6 +19,11 @@ def m_Linear(name, inp, out):
 
 def test_set_monitor():
     mnist = MNIST(("test", ))
+    return Flatten(DataStream.default_stream(mnist, 
+        iteration_scheme=SequentialScheme(mnist.num_examples, batch_size=1024)))
+
+def test_set_monitor_cifar():
+    mnist = CIFAR10(("test", ))
     return Flatten(DataStream.default_stream(mnist, 
         iteration_scheme=SequentialScheme(mnist.num_examples, batch_size=1024)))
 
@@ -46,7 +51,7 @@ def Custom_MLP():
     loop.run()
 
 def Custom_MLP2():
-    mnist = MNIST(("train",))
+    mnist = CIFAR10("train",)
     x = T.matrix('features')
     y = T.lmatrix('targets')
     layer1 = m_Linear("hidden", 784, 300)
@@ -74,13 +79,26 @@ def Custom_MLP2():
     loop.run()
 
 def Custom_conv():
-    mnist = MNIST(("train", ))
+    mnist =CIFAR10(("train", ))
     x = T.tensor4('features')
     y = T.lmatrix('targets')
     act = Rectifier().apply
-    conv1 = ConvolutionalLayer(act, (32,32), 3, (2,2), 4, image_size=(28,28), batch_size=256,
-        weights_init = IsotropicGaussian(.1), biases_init = Constant(1.))
+    conv1 = ConvolutionalLayer(act, (32,32), 3, (2,2), 4, image_size=(32,32), batch_size=256, name='conv1',
+        biases_init=Constant(0), weights_init=Constant(1.), tied_biases=True)
     conv1.initialize()
     res = conv1.apply(x)
+    flat = Flattener()
+    out = flat.apply(res)
+    mlp = MLP(activations=[Logistic(), Softmax()], dims=[2056, 100, 10])
+    loss = Softmax().categorical_cross_entropy(y, mlp.apply(out))
+    gr = ComputationGraph(loss)
+    monitor = DataStreamMonitoring(variables=[loss], data_stream=test_set_monitor_cifar())
+    data_stream = Flatten(DataStream.default_stream(mnist, 
+        iteration_scheme=SequentialScheme(mnist.num_examples, batch_size=256)))
+    algorithm = GradientDescent(cost=loss, step_rule=AdaDelta(), params=gr.parameters)
+    loop = MainLoop(data_stream=data_stream, algorithm=algorithm, extensions=[monitor, Printing()])
+    loop.run()
+    #func = theano.function([x], res)
     #seq = ConvolutionalSequence([conv1], 3, image_size=(28,28))
-    #mlp = MLP(activations=[Softmax()], dims=[2056, 50]).apply([seq])
+
+Custom_conv()
