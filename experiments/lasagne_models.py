d61992d865fff721aef01683273559aae7ebe760
Sergey Romanov <xxsmotur@gmail.com>
2015-08-18 16:03:45 -0700

    Update

--- experiments/lasagne_models.py
+++ experiments/lasagne_models.py
@@ -15,3 +15,12 @@ def l_mlp():
 
     predict = lasagne.layers.get_output(out)
     loss = lasagne.objectives.categorical_crossentropy(predict, target)
+    loss = loss.mean()
+    test_predict = lasagne.layers.get_output(out)
+    test_loss = lasagne.objectives.categorical_crossentropy(predict, target)
+    test_loss = test_loss.mean()
+    test_acc = T.mean(T.eq(T.argmax(test_predict, axis=1), target), dtype=theano.config.floatX)
+    params = lasagne.layers.get_all_params(out, trainable=True)
+    updates = lasagne.updates.nesterov_momentum(loss, params, learning_rate=0.01, momentum=0.9)
+    train_fn = theano.function([inpd, target], loss, updates=updates)
+    val_fn = theano.function([inpd, target], [test_loss, test_acc])
