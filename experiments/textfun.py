2735a9567a93ed284e6d4a59fb826e56bd5fd87b
Sergey Romanov <xxsmotur@gmail.com>
2015-10-22 04:45:23 -0700

    Add comment

--- experiments/textfun.py
+++ experiments/textfun.py
@@ -3,6 +3,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 import numpy as np
 
+#Examples of classification text with sklearn
+
 class NewsClassify:
     def __init__(self, method=RandomForestClassifier):
        self.train_data = fetch_20newsgroups(subset='train', categories=['comp.graphics', 'rec.sport.hockey',
@@ -25,3 +27,8 @@ class NewsClassify:
         dock_new = self.count_vec.transform(newdata)
         X_new = self.tfidf.transform(dock_new)
         return self.model.predict(X_new)
+
+'''model = NewsClassify()
+model.train()
+print(model.score())
+print(model.predict(['This team win six games at the raw']))'''
